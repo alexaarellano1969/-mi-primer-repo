a9952e3843b6228113a3678ba096956f0998bf0d
alexaarellano1969 <alexaarellano1969@gmail.com>
2025-05-16 22:27:31 -0700

    Entrada

--- Principal.java
+++ Principal.java
@@ -1,68 +1,12 @@
-import java.util.Scanner;
 public class Principal {
-    public static void main (String[] args){
-        CajeroAutomatico cajeroBancamexCajeroAutomatico( salhhjklñ; //inicializa en 1000 mi caieno automatico
-        Scanner sc = new Scanner (System. in);
-        int choice;
-        dot
-        CajeroAutomatico cajeroBBVA;
-        cajeroBBVA.mostrarMenu);
-        System. out println ("Elige una opcion del menu") ;
-        choice = sc.nextIntO;
-        switch(choice){
-            case
-                    1:
+    public static void main(String[] args) {
+        Entrada entrada1 = new Entrada( "Concierto de Tewnty One Pilots", 800.00);
+        Entrada entrada2 = new Entrada("Obra de teatro el lago de los cisnes", 450.50);
+        Entrada entrada3 = new Entrada("Cine de terror", 150.00);
 
+        entrada1.mostrarInformacion();
+        entrada2.mostrarInformacion();
+        entrada3.mostrarInformacion();
 
-
-                break;
-            case 2:
-
-
-
-
-                double deposito = sc.nextDouble();
-
-                cajeroBBVA. depositarDinero (deposito);
-
-                break;
-            case
-                    3:
-
-
-
-                double retino = sc. nextDouble();
-
-                cajeroBBVA. retirarDinero(retiro);
-
-                break;
-            default:
-
-
-
-
-                break;
-            System.out println("Su salda actual es de: $" + cajeroBBVA. consultarSaldo()) ;
-            System.out print ("Ingresa la cantidad que quieres depositar: ");
-            System.out print ("Ingresa la cantidad que quieres retinar: ");
-            System.out println("Bye bye! gracias por usar el cajero de BBVA
-            申务”）；
-        }
     }
-while(choice != 4);
-    sc. close;
 }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
